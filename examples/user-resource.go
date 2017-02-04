c36cb6c7706d7dc815ddcf7a48001abba3c35377
Ernest Micklei <emicklei@Ernests-iMac-10.local>
2017-02-04 12:31:33 +0100

    add working, but incomplete, example

---
+++ examples/user-resource.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	restfulspec "github.com/emicklei/go-restful-openapi"
+	"github.com/go-openapi/spec"
+)
+
+type UserResource struct {
+	// normally one would use DAO (data access object)
+	users map[string]User
+}
+
+func (u UserResource) Register() {
+	ws := new(restful.WebService)
+	ws.
+		Path("/users").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML) // you can specify this per route as well
+
+	ws.Route(ws.GET("/").To(u.findAllUsers).
+		// docs
+		Doc("get all users").
+		Writes([]User{}).
+		Returns(200, "OK", nil))
+
+	ws.Route(ws.GET("/{user-id}").To(u.findUser).
+		// docs
+		Doc("get a user").
+		Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")).
+		Writes(User{}). // on the response
+		Returns(404, "Not Found", nil))
+
+	ws.Route(ws.PUT("/{user-id}").To(u.updateUser).
+		// docs
+		Doc("update a user").
+		Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")).
+		Reads(User{})) // from the request
+
+	ws.Route(ws.PUT("").To(u.createUser).
+		// docs
+		Doc("create a user").
+		Reads(User{})) // from the request
+
+	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser).
+		// docs
+		Doc("delete a user").
+		Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")))
+
+	restful.Add(ws) // to the default container
+}
+
+// GET http://localhost:8080/users
+//
+func (u UserResource) findAllUsers(request *restful.Request, response *restful.Response) {
+	list := []User{}
+	for _, each := range u.users {
+		list = append(list, each)
+	}
+	response.WriteEntity(list)
+}
+
+// GET http://localhost:8080/users/1
+//
+func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("user-id")
+	usr := u.users[id]
+	if len(usr.Id) == 0 {
+		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+	} else {
+		response.WriteEntity(usr)
+	}
+}
+
+// PUT http://localhost:8080/users/1
+// <User><Id>1</Id><Name>Melissa Raspberry</Name></User>
+//
+func (u *UserResource) updateUser(request *restful.Request, response *restful.Response) {
+	usr := new(User)
+	err := request.ReadEntity(&usr)
+	if err == nil {
+		u.users[usr.Id] = *usr
+		response.WriteEntity(usr)
+	} else {
+		response.WriteError(http.StatusInternalServerError, err)
+	}
+}
+
+// PUT http://localhost:8080/users/1
+// <User><Id>1</Id><Name>Melissa</Name></User>
+//
+func (u *UserResource) createUser(request *restful.Request, response *restful.Response) {
+	usr := User{Id: request.PathParameter("user-id")}
+	err := request.ReadEntity(&usr)
+	if err == nil {
+		u.users[usr.Id] = usr
+		response.WriteHeaderAndEntity(http.StatusCreated, usr)
+	} else {
+		response.WriteError(http.StatusInternalServerError, err)
+	}
+}
+
+// DELETE http://localhost:8080/users/1
+//
+func (u *UserResource) removeUser(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("user-id")
+	delete(u.users, id)
+}
+
+func main() {
+	u := UserResource{map[string]User{}}
+	u.Register()
+
+	// Optionally, you can install the Swagger Service which provides a nice Web UI on your REST API
+	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
+	// Open http://localhost:8080/apidocs and enter http://localhost:8080/apidocs.json in the api input field.
+	config := restfulspec.Config{
+		WebServices:    restful.RegisteredWebServices(), // you control what services are visible
+		WebServicesURL: "http://localhost:8080",
+		APIPath:        "/apidocs.json",
+		Info: spec.Info{
+			InfoProps: spec.InfoProps{
+				Title:       "UserService",
+				Description: "Resource for managing Users",
+				Contact: &spec.ContactInfo{
+					Name:  "john",
+					Email: "john@doe.rp",
+					URL:   "http://johndoe.org",
+				},
+				License: &spec.License{
+					Name: "MIT",
+					URL:  "http://mit.org",
+				},
+				Version: "1.0.0",
+			},
+		},
+		// Optionally, specifiy where the Swagger UI is located
+		SwaggerPath:     "/apidocs/",
+		SwaggerFilePath: "/Users/emicklei/Projects/swagger-ui/dist"}
+	restfulspec.RegisterOpenAPIService(config, restful.DefaultContainer)
+
+	log.Printf("start listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type User struct {
+	Id, Name string
+}
--- spec_resource.go
+++ spec_resource.go
@@ -1,12 +1,55 @@
 package restfulspec
 
 import restful "github.com/emicklei/go-restful"
+import "github.com/go-openapi/spec"
+
+// RegisterOpenAPIService adds the WebService that provides the API documentation of all services
+// conform the OpenAPI documentation specifcation.
+func RegisterOpenAPIService(config Config, wsContainer *restful.Container) {
+
+	ws := new(restful.WebService)
+	ws.Path(config.APIPath)
+	ws.Produces(restful.MIME_JSON)
+	if config.DisableCORS {
+		ws.Filter(enableCORS)
+	}
+
+	res := specResource{config: config, paths: spec.Paths{Paths: map[string]spec.PathItem{}}}
+	ws.Route(ws.GET("/").To(res.getSwagger))
+
+	// TEMP
+	for _, each := range config.WebServices {
+		po := buildPaths(each)
+		for path, item := range po.Paths {
+			res.paths.Paths[path] = item
+		}
+	}
+	wsContainer.Add(ws)
+}
+
+func enableCORS(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+	if origin := req.HeaderParameter(restful.HEADER_Origin); origin != "" {
+		// prevent duplicate header
+		if len(resp.Header().Get(restful.HEADER_AccessControlAllowOrigin)) == 0 {
+			resp.AddHeader(restful.HEADER_AccessControlAllowOrigin, origin)
+		}
+	}
+	chain.ProcessFilter(req, resp)
+}
 
 // specResource is a REST resource to serve the Open-API spec.
 type specResource struct {
 	config Config
+	paths  spec.Paths
 }
 
-// RegisterOpenAPIService add the WebService that provides the API documentation of all services
-// conform the OpenAPI documentation specifcation.
-func RegisterOpenAPIService(config Config, wsContainer *restful.Container) {}
+func (s specResource) getSwagger(req *restful.Request, resp *restful.Response) {
+	sw := spec.Swagger{
+		SwaggerProps: spec.SwaggerProps{
+			Info:    &s.config.Info,
+			Swagger: "2.0",
+			Paths:   &(s.paths),
+		},
+	}
+	resp.WriteAsJson(sw)
+}
